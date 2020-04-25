b9c1f8496523e2eb43d71506bcaf21678d1e0a7f
maxkde <max@studio-4711.com>
2020-04-25 02:32:42 +0200

    CHG: Expose hardware error and hardware reset events from robot to preconfigured robot as well

--- Libmirobot/Libmirobot/PreconfiguredRobot.cs
+++ Libmirobot/Libmirobot/PreconfiguredRobot.cs
@@ -46,11 +46,31 @@ namespace Libmirobot
         /// <summary>
         /// Event being fired, when the robot component detects a status update.
         /// </summary>
-        /// <remarks>Will fire when the hardware's response for the status request. Status request can be triggered via UpdateCurrentPosition() method or by setting AutoSendStatusUpdateRequests property to true.</remarks>
+        /// <remarks>Will fire when the hardware's response for the status request arrives. Status request can be triggered via UpdateCurrentPosition() method or by setting AutoSendStatusUpdateRequests property to true.</remarks>
         public event EventHandler<RobotStateChangedEventArgs> RobotStateChanged
         {
             add { this.Robot.RobotStateChanged += value; }
             remove { this.Robot.RobotStateChanged -= value; }
+        }
+
+        /// <summary>
+        /// Event being fired, when the hardware reports an incident, such as an alarm or a triggered limit.
+        /// </summary>
+        /// <remarks>Manual user interaction may be required, depending on the error.</remarks>
+        public event EventHandler<RobotErrorEventArgs> RobotErrorOccured
+        {
+            add { this.Robot.RobotErrorOccurred += value; }
+            remove { this.Robot.RobotErrorOccurred -= value; }
+        }
+
+        /// <summary>
+        /// Event being fired, when the hardware reports a reset.
+        /// </summary>
+        /// <remarks>Re-performing the homing operation may be necessary (but can be handled automatically).</remarks>
+        public event EventHandler<RobotResetEventArgs> RobotResetOccurred
+        {
+            add { this.Robot.RobotResetOccurred += value; }
+            remove { this.Robot.RobotResetOccurred -= value; }
         }
 
         /// <summary>
